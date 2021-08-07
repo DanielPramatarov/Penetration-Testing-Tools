0470ee32bb6442d8dff623eab1cd62d35e2ec63a
DanielPramatarov <danielpramatarov@gmail.com>
2021-08-07 10:33:56 +0300

    SubEnum

--- Python/Subdomain Enumeration/SubEnum.py
+++ Python/Subdomain Enumeration/SubEnum.py
@@ -5,7 +5,7 @@ sub_list = open("subdomains.txt").read()
 subdoms = sub_list.splitlines()
 
 for sub in subdoms:
-    sub_domains = f"http://{sub}.tesla.com" 
+    sub_domains = f"http://{sub}.test.com" 
 
     try:
         requests.get(sub_domains)
